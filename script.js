8feb0dee7e6cf51171ebfc01e6a2f929f34c4085
Joni Korpi <me@jonikorpi.com>
2012-09-16 19:10:58 +0300

    Removes heights, paddings and headings

--- script.js
+++ script.js
@@ -33,15 +33,15 @@ bm_ns.delete_row = function(e) {
 }
 
 bm_ns.generate_bookmarklet = function(highlight) {
-	var link = "javascript:document.write('<!DOCTYPE html><html><head>&lt;meta charset=&quot;utf-8&quot;><title>Responsive Design Testing</title><style>body { margin: 20px; font-family: sans-serif; overflow-x: scroll; }.wrapper { width: 6000px; }.frame { float: left; }h2 { margin: 0 0 5px 0; }iframe { margin: 0 20px 20px 0; border: 1px solid #666; }</style></head><body><div class=&quot;wrapper&quot;>";
+	var link = "javascript:document.write('<!DOCTYPE html><html><head>&lt;meta charset=&quot;utf-8&quot;><title>Responsive Design Testing</title><style>html{min-height: 100%;} body{ height: 100%; position: relative; margin: 0; overflow-x: scroll; background: #000; }.wrapper { width: 20000px; }.frame { float: left; background: #fff; position: relative; border-left: 6px solid rgb(158, 158, 158);}h2 { margin: 0; }iframe { margin: 0; border: none;}</style></head><body><div class=&quot;wrapper&quot;>";
 	$("#bm_generate .sortable .row").each(function() {
 		var width  = $(this).find(".col1 input").val();
-		var height = $(this).find(".col2 input").val();
+		var height = '100%';
 		var label  = $(this).find(".col3 input").val();
 		if (label) {
 			label = "(" + label + ")";
 		}
-		link += "<div class=&quot;frame&quot;><h2>" + width + "<span> x " + height + "</span> <small>" + label + "</small></h2>"
+		link += "<div class=&quot;frame&quot;>"
 					+ "<iframe src=&quot;' + window.location + '&quot; sandbox=&quot;allow-same-origin allow-forms&quot; seamless "
 					+ "width=&quot;" + width + "&quot; height=&quot;" + height + "&quot;></iframe></div>"
 	});
