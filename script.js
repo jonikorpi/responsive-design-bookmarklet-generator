f4b5d1abfd0ac2884a4c3e16b9a8a6dcb6bed063
Joni Korpi <me@jonikorpi.com>
2012-09-17 13:51:06 +0300

    Still broken goddamnit

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+/*
+  --------------------------
+  Stuff for the generator UI
+  --------------------------
+*/
+
 $(function() {
 	$("#bm_generate .sortable").sortable({
 		axis:   "y",
@@ -32,49 +38,46 @@ bm_ns.delete_row = function(e) {
 	$(e.target).closest(".row").remove();
 }
 
-
-
-
-
-
-
+/*
+  -----------------
+  Bookmarklet stuff
+  -----------------
+*/
 
 bm_ns.generate_bookmarklet = function(highlight) {
-	var link = "\
-	javascript:document.write('\
-    <div id=&quot;rdtb-wrapper&quot;>\
-    	<style>\
-        html{height: 100% !important;} \
-        body {position: relative !important; height: 100% !important; padding: 0 !important; margin: 0 !important;} \
-        #rdtb-wrapper{ height: 100%; position: absolute; top: 0; bottom: 0; left: 0; right: 0; margin: 0; overflow: auto; width: 1250em;}\
-        .rdtb-frame { float: left; background: #fff; position: relative; border-left: 6px solid rgb(158, 158, 158);}\
-        .rdtb-frame iframe { margin: 0; border: none;}\
-      </style>";
-  	
-  	$("#bm_generate .sortable .row").each(function() {
-  		var width  = $(this).find(".col1 input").val();
-  		var height = '100%';
-  		var label  = $(this).find(".col3 input").val();
-  		if (label) {
-  			label = "(" + label + ")";
-  		}
-  		link += "<div class=&quot;rdtb-frame&quot;>"
-  					+ "<iframe src=&quot;' + window.location + '&quot; allow-forms&quot; seamless "
-  					+ "width=&quot;" + width + "&quot; height=&quot;" + height + "&quot;></iframe></div>"
-  	});
+  
+  // Create markup
+	var markup = "\
+  	<style>\
+      html{height: 100% !important;} \
+      body {position: relative !important; height: 100% !important; padding: 0 !important; margin: 0 !important;} \
+      #rdtb-wrapper{ height: 100%; position: absolute; top: 0; bottom: 0; left: 0; right: 0; margin: 0; overflow: auto; width: 1250em; z-index: 9999999999;}\
+      .rdtb-frame { float: left; background: #fff; position: relative; border-left: 6px solid rgb(158, 158, 158);}\
+      .rdtb-frame iframe { margin: 0; border: none;}\
+    </style>";
+    
+	// Add iframes based on selected sizes
+	$("#bm_generate .sortable .row").each(function() {
+		var width  = $(this).find(".col1 input").val();
+		var height = '100%';
+		markup += "<div class=&quot;rdtb-frame&quot;>"
+					+ "<iframe src=&quot;' + window.location + '&quot; allow-forms&quot; seamless "
+					+ "width=&quot;" + width + "&quot; height=&quot;" + height + "&quot;></iframe></div>"
+	});
+    
+  // Splurge everything into a script
+  var link = 'javascript:' + 
+  'document.getElementsByTagName(body)[1]' +
+    '.appendChild(' + 
+      'document.createElement(&quot;div&quot;)' + 
+        '.setAttribute(&quot;id&quot;, &quot;rdtb-wrapper&quot;)' + 
+        '.innerHTML(&quot;'+ markup +'&quot;)' + 
+    ');';
 	
-  	link += "</div>\
-  ')";
-	
-	
-	
-	
-	
-	
-	
-	
+	// Add script into the bookmarklet
 	$("#bookmarklet_link").html('<a href="' + link + '">Responsive Design Test</a>');
-
+	
+	// Animate link to show that script generation worked
 	if (highlight) {
 		$("#bookmarklet_link").effect("highlight", { color: "#00ff00" }, 2000);
 	}
